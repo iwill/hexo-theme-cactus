e504ce816f7065968889860a0fbc81381c184150
Míng <minglq.9@gmail.com>
2023-04-26 10:45:35 +0800

    fea: add rev94

--- source/js/main.js
+++ source/js/main.js
@@ -126,11 +126,19 @@ $(document).ready(function() {
     
     if (/\/about\b/.test(location.pathname)) {
       
+      function rev94(s) {
+        return s.replace(/./g, (c) => {
+          let d = c.charCodeAt(0);
+          if (d <= 32 || d >= 127) return c;
+          return String.fromCharCode((33 + 126) - d);
+        });
+      }
+      
       function evenOdd(s) {
           return s.replace(/./g, (c) => {
               let d = c.charCodeAt(0);
               if (d < 32 || d == 64 || d >= 127) return c;
-              return String.fromCharCode(d + (d % 2 || -1) * (d > 64 || -1));
+              return String.fromCharCode(d + (d % 2 || -1) * (d > 64 ? 1 : -1));
           });
       }
       
@@ -147,15 +155,17 @@ $(document).ready(function() {
         clearTimeout(displayTimeout);
         
         let text = this.getAttribute("data-m");
-        let mask = evenOdd(text).replaceAll("+", "#");
+        let mask = rev94(text).replaceAll("+", "#");
+        // let text = this.getAttribute("data-m");
+        // let mask = evenOdd(text).replaceAll("+", "#");
         let ori = this.innerHTML;
         this.innerHTML = mask;
         let copy = this.innerText;
         this.innerHTML = ori;
         
         navigator.clipboard.writeText(copy).then(
-          () => display("copied"),
-          () => display("failed to copy")
+          () => display("copied!"),
+          () => display("failed to copy!")
         );
         
         return false;
